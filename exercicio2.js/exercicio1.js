00f304d802c16e9bc45edfd6ee9bdd4ca8af72e6
Marcelo Barbosa <marcelobarbosafb@gmail.com>
2023-02-15 22:06:01 -0300

    exercicio de hora com imagem
    este código faz carregar a pagina com horário atualizado de cada maquina onde o código roda , mudando a cor de fundo e imagens de acordo com a hora local.

--- exercicio2.js/exercicio1.js
+++ exercicio2.js/exercicio1.js
@@ -1,1 +1,27 @@
-alert("ola")
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = ` Agora são ${hora} horas`
+    document.body.style.fontSize ='20px'
+    
+    if(hora >= 0 && hora <12){
+        
+        img.src ='./img/manhã.jpg'
+        document.body.style.background ='#d6421d'
+    }
+        else if(hora >= 12 && hora <= 18){
+            img.src ='./img/tarde.jpg'
+            document.body.style.background ='#c3234c'
+        }
+        else{
+            img.src ='./img/noite.jpg'
+            document.body.style.background ='#000'
+        }
+
+    
+
+}
+   // este codigo faz carregar a pagina com horario atualizado de cada maquina onde o codigo roda , mudando a cor de fundo e imagens de acordo com a hora local.
