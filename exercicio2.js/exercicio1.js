ffbbcb0dea3972686a631a3147579c863481b919
Marcelo Barbosa <marcelobarbosafb@gmail.com>
2023-03-01 19:38:54 -0300

    condições

--- exercicio2.js/exercicio1.js
+++ exercicio2.js/exercicio1.js
@@ -2,23 +2,23 @@ function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    
+   // var hora = data.getHours()
+    var hora = 22
     msg.innerHTML = ` Agora são ${hora} horas`
     document.body.style.fontSize ='20px'
     
     if(hora >= 0 && hora <12){
         
         img.src ='./img/manhã.jpg'
-        document.body.style.background ='#d6421d'
+        document.body.style.background ='#d6421d';
     }
         else if(hora >= 12 && hora <= 18){
             img.src ='./img/tarde.jpg'
-            document.body.style.background ='#c3234c'
+            document.body.style.background ='#c3234c';
         }
         else{
             img.src ='./img/noite.jpg'
-            document.body.style.background ='#000'
+            document.body.style.background ='#000';
         }
 
     
